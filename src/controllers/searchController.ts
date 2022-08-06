26fdf46b21a6fc69093ace8af00a2f16db44862d
JromarioSs <jromario2014@gmail.com>
2022-08-06 11:17:23 -0300

    update1.2

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -20,6 +20,34 @@ export const buscar = async (req: Request, res: Response) => {
   res.render('pages/search', { finded, showUser });
 }
 
+export const editar = async (req: Request, res: Response) => {
+  let nameFind: string = req.query.nomeSrc as string;
+  let showUser: boolean = false;
+  let finded = await User.findAll({
+    where: {
+      nome: {
+        [Op.like]: `%${nameFind}%`
+      }
+    }
+  });
+
+  if (nameFind) {
+    showUser = true;
+  }
+
+  res.render('pages/edit', { finded, showUser });
+}
+
+export const editarUser = async (req: Request, res: Response) => {
+  res.render('/pages/edit');
+}
+
+export const excluir = async (req: Request, res: Response) => {
+  let id: string = req.params.id;
+  await User.destroy({ where: { id } });
+  res.redirect('/pesquisar');
+}
+
 export const mostrarTodoUser = async (req: Request, res: Response) => {
   let users = await User.findAll();
   res.render('pages/showAll', { users });
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,9 +6,16 @@ import * as SearchController from '../controllers/searchController';
 const router = Router();
 
 router.get('/', HomeController.home);
+
 router.get('/cadastrar', UserController.cadastrar);
 router.post('/cadastrarNv', UserController.cadastrarNv);
+
 router.get('/pesquisar', SearchController.buscar);
+router.get('/pesquisar/:id/excluir', SearchController.excluir);
+
+router.get('/editar', SearchController.editar);
+router.put('/editarUser', SearchController.editarUser);
+
 router.get('/todosUsers', SearchController.mostrarTodoUser);
 
 export default router;
